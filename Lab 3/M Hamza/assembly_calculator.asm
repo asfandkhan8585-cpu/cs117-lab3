ab22f051671d3a1778bd7750d5505bbe945ce464
M-Hamza01 <mhamza9526322@gmail.com>
2025-09-29 23:23:20 +0500

    Update assembly_calculator.asm

--- Lab 3/M Hamza/assembly_calculator.asm
+++ Lab 3/M Hamza/assembly_calculator.asm
@@ -1,42 +1,37 @@
+;this program  add and multiply two 
+;vlues which are hard coded in it. 
+
 section .data
   msg1    db 'Addition RESULT: ',0  
-  
-  msg1Len equ $-msg1 
-	
-  add_result db 0 
-
-  msg2    db 'Multiplication RESULT: ',0   
-  
+ msg1Len equ $-msg1 
+	 add_result db 0 
+ msg2    db 'Multiplication RESULT: ',0   
   msg2Len equ $-msg2 
-	
-  mul_result db 0 
+mul_result db 0 
+
+
+
 
 section .text
   global _start
 
 _start:
+mov al, 4
+mov bl, 2
 
-   mov al, 4
-   mov bl, 2
-
-      add al, 2              
-   add al, '0'           
-  
-   mov [add_result], al
-
-  
+   add al, 2              
+add al, '0'        
+mov [add_result], al
   
    mov al, 4 
-  
-  mul bl   
-  
-  add al, '0'           
-  mov [mul_result], al
-  
-  mov eax,4
-   mov ebx,1
+ mul bl   
+add al, '0'  
+
+mov [mul_result], al
+mov eax,4
+mov ebx,1
   mov ecx,msg1
-   mov edx,msg1Len
+  mov edx,msg1Len
   int 0x80
 
   mov eax,4
@@ -59,4 +54,5 @@ _start:
 
   mov eax,1
   mov ebx,0
+
   int 0x80
